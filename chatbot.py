a95d5a5ae54483e48306c94a1185a735ddbe152f
linusedman <linus.edman33@gmail.com>
2025-10-19 13:33:43 +0200

    moved the faiss index path to the env file and updated the example

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 15 11:06:24 2025
-Updated on Sat Oct 18 17:17:00 2025
+Updated on Sun Oct 19 13:32:50 2025
 
 @author: Lovisa
 @coauthor: Agnes
@@ -19,6 +19,7 @@ from PIL import Image
 
 load_dotenv()
 KEY = os.environ.get("GEMINI_API_KEY")
+FAISS_INDEX = os.environ.get("FAISS_FOLDER")
 
 client = genai.Client(api_key=KEY)
 
@@ -29,7 +30,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(model_name="KBLab/sentence-bert-swedish-cased")
 
-db = FAISS.load_local("faiss_index_all_sv_cs1000", embeddings, allow_dangerous_deserialization=True)  # Load the vector database
+db = FAISS.load_local(FAISS_INDEX, embeddings, allow_dangerous_deserialization=True)  # Load the vector database
 
 def response_stream(inputs, history):
     user_text = ""
